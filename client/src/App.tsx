5e96f2ac91579bbb09be166c0eea438a8e7dd005
Sshady1545 <shady15452shady@gmail.com>
2026-02-04 21:43:40 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   const [secretInput, setSecretInput] = useState('');
   const [secretMessage, setSecretMessage] = useState('');
 
-  const SERVER_IP = 'skibiditoilet.shock.gg';
+  const SERVER_IP = 'skibiditoilet.shock.gg:25568';
 
   useEffect(() => {
     const createParticles = () => {
